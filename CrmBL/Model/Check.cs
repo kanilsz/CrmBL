f2634f5982a952701460e1cfab3d5672c84d6108
Maksym Konotop <konotop2001@gmail.com>
2020-06-12 00:01:11 +0300

    Created all model classes

---
+++ CrmBL/Model/Check.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int SellerId { get; set; }
+        public virtual Seller Seller{ get; set; }
+
+    }
+}
---
+++ CrmBL/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public String Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
