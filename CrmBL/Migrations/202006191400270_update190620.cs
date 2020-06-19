7c6faf8994445e7636e7f2d7699b982356fcce1c
Maksym Konotop <konotop2001@gmail.com>
2020-06-19 17:08:47 +0300

    Update

---
+++ CrmBL/Migrations/202006191400270_update190620.cs
@@ -0,0 +1,18 @@
+﻿namespace CrmBL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update190620 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Checks", "Summary", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Checks", "Summary");
+        }
+    }
+}
--- CrmUI/CustomerForm.cs
+++ CrmUI/CustomerForm.cs
@@ -20,7 +20,7 @@ namespace CrmUI
         }
         public CustomerForm(Customer customer) : this()
         {
-            Customer = customer;
+            Customer = customer ?? new Customer();
             textBox1.Text = customer.Name; 
         }
         private void CustomerForm_Load(object sender, EventArgs e)
@@ -30,8 +30,8 @@ namespace CrmUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var c = Customer ?? new Customer();
-            c.Name = textBox1.Text;
+            Customer = Customer ?? new Customer();
+            Customer.Name = textBox1.Text;
             Close();
         }
     }
